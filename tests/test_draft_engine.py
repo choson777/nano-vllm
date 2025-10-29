c5f07857887cca8830c0bacb3ad39b28d0a0af53
lqc <lqc152710868@gmail.com>
2025-10-29 16:09:14 +0800

    test draft model always correct

--- tests/test_draft_engine.py
+++ tests/test_draft_engine.py
@@ -1,5 +1,5 @@
 import os
-from nanovllm import SpeculativeEngine, LLM, DraftEngine, DraftConfig, SamplingParams
+from nanovllm import SpeculativeEngine, LLM, DraftEngine, DraftConfig, SamplingParams, Sequence
 from dataclasses import asdict
 from transformers import AutoTokenizer
 import torch
@@ -8,7 +8,7 @@ def main():
     draft_model_path = os.path.expanduser("/data3/lqc/models/qwen3-0.6b/")
     tokenizer = AutoTokenizer.from_pretrained(draft_model_path)
     draft_config = DraftConfig()
-    draft_llm = DraftEngine(draft_model_path, num_turn_spec_tokens=4, **asdict(draft_config))
+    draft_engine = DraftEngine(draft_model_path, num_turn_spec_tokens=4, **asdict(draft_config))
 
     sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
 
@@ -26,14 +26,21 @@ def main():
         )
         for prompt in prompts
     ]
-    for prompt in prompts:
-        draft_llm.add_request(prompt, sampling_params)
+    sampling_params = sampling_params = [sampling_params] * len(prompts)
+    seq_id_map = {}
+    for prompt, sp in zip(prompts, sampling_params):
+        if isinstance(prompt, str):
+            prompt = tokenizer.encode(prompt)
+        target_seq = Sequence(prompt, sp)
+        draft_seq = Sequence(prompt, sp)
+        seq_id_map[target_seq.seq_id] = draft_seq.seq_id
+        draft_engine.add_request(draft_seq)
 
         
     outputs = {}
     for i in range(10):
         print(i)
-        output, seq_logits = draft_llm.run()
+        output, seq_logits = draft_engine.run()
         print(output)
         for seq_id in output:
             if seq_id not in outputs:
